b7352370dff4b0528d0af3b86b9a6546784d1855
daemon.devin <daemon.devin@gmail.com>
2024-05-15 00:34:26 +0000

    Uploaded files. Work in progress..

--- public/scripts/Lobby.js
+++ public/scripts/Lobby.js
@@ -1,24 +1,20 @@
-import {
-    listenEnter
-} from './enterkey.js';
-import './modal.js'
-document.getElementById('NewGamebtn').addEventListener('click', createnewgame)
-
-
-
+import { listenEnter } from "./enterkey.js";
+document
+  .getElementById("NewGamebtn")
+  .addEventListener("click", createnewgame());
+document.getElementById("rulesBtn").addEventListener("click", () => {
+  $("#rules").toggle();
+});
 function createnewgame() {
-    
-    fetch('/api/newgame/')
-        .then((resp) => {
-            return resp.json()
-        }).then((data) => {
-            console.log(data);
-            window.location.href = window.location+data.Name
-            
-        })
-    //.then((data) => {console.log('wow')})
+  fetch("/api/newgame/")
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+      window.location.href = window.location + data.Name;
+    });
+  //.then((data) => {console.log('wow')})
 }
 
-
-
-listenEnter('NewGameName', createnewgame)
+document.getElementById("NewGameName", createnewgame());
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,188 +1,243 @@
-import {nameModal,playerdisconnect,simpleModal} from './modal.js'
-import "./invite.js";
-import { listenEnter } from './enterkey.js';
 //establish a socket connection to the server
 var socket = io(window.location.pathname);
 
 //Show the nameModal to allow users entering the room to give themselves a name.
-nameModal(true);
+//nameModal(true);
+let result = prompt(
+  "What's Your Name",
+  "Please enter your name to join the game..",
+);
 diceEventListeners();
 
+document.getElementById("copyInvite").addEventListener("click", () => {
+  console.log("copy modal opened");
+  $.toast({
+    showIcon: "copy",
+    displayTime: "auto",
+    showProgress: "top",
+    title: "Copied Invite Link!",
+    message: "You can now send the link to a friend!",
+    class: "black",
+    className: {
+      toast: "ui icon message",
+    },
+  });
+  let ta = document.createElement("textarea");
+  ta.value = document.location;
+  document.body.appendChild(ta);
+  ta.select();
+  document.execCommand("copy");
+  document.body.removeChild(ta);
+
+  /* setTimeout(function(){
+      copiedmodal(false)
+  },4000) */
+});
 
 function displayButton(id, show) {
-    if (show) {
-        document.getElementById(id).style.display = "inline-block";
-    } else {
-        document.getElementById(id).style.display = "none"
-    }
+  if (show) {
+    document.getElementById(id).style.display = "inline-block";
+  } else {
+    document.getElementById(id).style.display = "none";
+  }
 }
 
-
 function rand() {
-    return Math.floor(Math.random() * 6 + 1)
+  return Math.floor(Math.random() * 6 + 1);
+}
+
+function playerdisconnect(player) {
+  $.modal("alert", {
+    title: "Connection Lost",
+    message:
+      "Sorry " +
+      player.name +
+      " but you have lost the connection with the server. You will now be taken to the lobby as this game is over.",
+    handler: function (name) {
+      window.setTimeout(() => {
+        window.location.href = "../";
+      }, 1000 * 5);
+    },
+  });
 }
 
 function rollanim(dice) {
-    for(let i =0;i<=5;i++){
-        if(dice[i].avalible){
-            document.getElementById('d'+(i)).style.backgroundColor='white'
-        }
-        else{
-            document.getElementById('d'+(i)).style.backgroundColor='grey'
-        }
+  for (let i = 0; i <= 5; i++) {
+    if (dice[i].avalible) {
+      document.getElementById("d" + i).style.backgroundColor = "white";
+    } else {
+      document.getElementById("d" + i).style.backgroundColor = "grey";
     }
-    var changes = 0;
-    var interval = window.setInterval(function () {
-        changes++
+  }
+  var changes = 0;
+  var interval = window.setInterval(function () {
+    changes++;
 
-        for (let i = 0; i <= 5; i++) {
-            if(dice[i].avalible){
-                document.getElementById('d' + (i)).setAttribute('src','/img/dice'+rand()+'.svg')
-            }
-        }
-        if (changes > 30) {
-            for (let i = 0; i <= 5; i++) {
-                //let diceElem = document.createElement('svg')
-                //diceElem.setAttribute('src','/img/dice'+dice[i].value+".svg")
+    for (let i = 0; i <= 5; i++) {
+      if (dice[i].avalible) {
+        document
+          .getElementById("d" + i)
+          .setAttribute("src", "/img/dice" + rand() + ".svg");
+      }
+    }
+    if (changes > 30) {
+      for (let i = 0; i <= 5; i++) {
+        //let diceElem = document.createElement('svg')
+        //diceElem.setAttribute('src','/img/dice'+dice[i].value+".svg")
 
-                document.getElementById('d' + (i)).setAttribute('src','/img/dice'+dice[i].value+'.svg')
-        
-            }
-            window.clearInterval(interval)
-        }
-    }, 100)
-
+        document
+          .getElementById("d" + i)
+          .setAttribute("src", "/img/dice" + dice[i].value + ".svg");
+      }
+      window.clearInterval(interval);
+    }
+  }, 100);
 }
 
 function gamestart() {
-    socket.emit('GameStart')
-    displayButton('GameStart',false)
+  socket.emit("GameStart");
+  displayButton("GameStart", false);
 }
 
 function roll(dice) {
-    socket.emit('roll', dice)
+  socket.emit("roll", dice);
 }
 
 function Bank() {
-    socket.emit('bank')
+  socket.emit("bank");
 }
 
 function sendmsg() {
-    socket.emit('msg', document.getElementById('chatmessage').value)
-    let chatlog = document.getElementById('chatlog');
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode('You: '+document.getElementById('chatmessage').value))
-    chatlog.appendChild(li)
-    document.getElementById('chatmessage').value = ""
+  socket.emit("msg", document.getElementById("chatmessage").value);
+  let chatlog = document.getElementById("chatlog");
+  let li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(
+      "You: " + document.getElementById("chatmessage").value,
+    ),
+  );
+  chatlog.appendChild(li);
+  document.getElementById("chatmessage").value = "";
 }
-socket.on('msg', function (msg) {
-    console.log(msg)
-    let chatlog = document.getElementById('chatlog');
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(msg.sender + ": " + msg.msg))
-    //to do: rewrite this so on a new message coming in we make a new <li>
-    chatlog.appendChild(li)
-})
-socket.on('connection', console.log('Newconnections'))
+socket.on("msg", function (msg) {
+  console.log(msg);
+  let chatlog = document.getElementById("chatlog");
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(msg.sender + ": " + msg.msg));
+  //to do: rewrite this so on a new message coming in we make a new <li>
+  chatlog.appendChild(li);
+});
+socket.on("connection", console.log("Newconnections"));
 
 function join(playername) {
-    socket.emit('playerjoin', {
-        name: playername
-    });
-    nameModal(false)
+  socket.emit("playerjoin", {
+    name: playername,
+  });
+  //nameModal(false)
 }
-socket.on('playerupdate', (players,turnindex) => {
-    //in production app we should NOT share socket ID's
-    document.getElementById('PlayerList').innerHTML = "";
-    console.log(players)
-    if (players.filter((player) => player.host)[0].id != socket.id) {
-        console.log('player is not host!')
-        displayButton('GameStart', false)
+socket.on("playerupdate", (players, turnindex) => {
+  //in production app we should NOT share socket ID's
+  document.getElementById("PlayerList").innerHTML = "";
+  console.log(players);
+  if (players.filter((player) => player.host)[0].id != socket.id) {
+    console.log("player is not host!");
+    displayButton("GameStart", false);
+  }
+  let playerlist = document.getElementById("PlayerList");
+  for (let i = 0; i < players.length; i++) {
+    console.log(players[i]);
+    let li = document.createElement("li");
+    let name = document.createElement("div");
+    let score = document.createElement("div");
+
+    li.appendChild(name);
+    li.appendChild(score);
+    if (i == turnindex) {
+      name.innerHTML = ">" + players[i].name;
+    } else {
+      name.innerHTML = players[i].name;
     }
-    let playerlist = document.getElementById('PlayerList');
-    for (let i = 0; i < players.length; i++) {
-        console.log(players[i])
-        let li = document.createElement('li');
-        let name = document.createElement('div');
-        let score = document.createElement('div');
+    score.innerHTML = players[i].score;
+    name.setAttribute("id", "PlayerListName");
+    score.setAttribute("id", "PlayerListScore");
 
-        li.appendChild(name);
-        li.appendChild(score);
-        if(i==turnindex){
-        name.innerHTML = ">"+players[i].name
-        }
-        else{
-        name.innerHTML = players[i].name}
-        score.innerHTML = players[i].score
-        name.setAttribute('id', 'PlayerListName')
-        score.setAttribute('id', 'PlayerListScore')
+    playerlist.appendChild(li);
+  }
+});
 
-        playerlist.appendChild(li)
-    }
-})
+socket.on("newturn", (player) => {
+  ismyturn = player.id == socket.id;
+  if (ismyturn) {
+    diceindex = [0, 1, 2, 3, 4, 5];
+  } else {
+    displayButton("Bank", false);
+  }
+  displayButton("RollBtn", ismyturn);
+});
+socket.on("roll_Return", function (dice) {
+  console.log(dice);
+  diceLocal = dice;
+  //avalibleDice = dice.filter(x=>{console.log(x);return x.avalible}).map((y,i)=>{return i})
+  rollanim(dice);
+  displayButton("Bank", ismyturn);
+});
+socket.on("gamewon", (player) => {
+  console.log("game was won by " + player.name);
+  $.modal("alert", {
+    transition: "Fade Up",
+    class: "small inverted",
+    message: "The winner is " + player.name + "!",
+  });
+  window.setTimeout(() => {
+    window.location.href = "../";
+  }, 1000 * 5);
+});
+socket.on("playerDisconect", function (player) {
+  playerdisconnect(player);
+});
 
-socket.on('newturn', (player) => {
-
-    ismyturn= player.id==socket.id
-    if(ismyturn){
-        diceindex = [0, 1, 2, 3, 4, 5];
-    }
-    else{
-        displayButton('Bank',false)
-    }
-    displayButton('RollBtn', ismyturn)
-    
-
-})
-socket.on('roll_Return', function (dice) {
-    console.log(dice);
-    diceLocal=dice
-    //avalibleDice = dice.filter(x=>{console.log(x);return x.avalible}).map((y,i)=>{return i})
-    rollanim(dice)
-    displayButton('Bank', ismyturn)
-})
-socket.on('gamewon', (player)=>{
-    console.log('game was won by '+player.name)
-    simpleModal('The Game Was Won by '+player.name,5);
-    window.setTimeout(()=>{window.location.href = '../'},1000*5)
-
-})
-socket.on('playerDisconect', function (player){
-    playerdisconnect(player);
-})
-
-var diceLocal=[]
-var diceindex = [0,1,2,3,4,5];
+var diceLocal = [];
+var diceindex = [0, 1, 2, 3, 4, 5];
 var ismyturn = false;
 
-document.getElementById('joinBtn').addEventListener('click', function () {
-    join(document.getElementById('playername').value)
-})
-document.getElementById('GameStart').addEventListener('click',function(){gamestart()})
+document.getElementById("joinBtn").addEventListener("click", function () {
+  join(document.getElementById("playername").value);
+});
+document.getElementById("GameStart").addEventListener("click", function () {
+  gamestart();
+});
 
-document.getElementById('RollBtn').addEventListener('click',function(){socket.emit('roll', diceindex)})
-document.getElementById('Bank').addEventListener('click',function(){Bank()})
+document.getElementById("rollBtn").addEventListener("click", function () {
+  socket.emit("roll", diceindex);
+});
+document.getElementById("Bank").addEventListener("click", function () {
+  Bank();
+});
 //For sending Chat mesages
-document.getElementById('sendChatMsg').addEventListener('click',function(){sendmsg()})
-listenEnter('playername',function () {
-    join(document.getElementById('playername').value)
-})
-listenEnter('chatmessage',sendmsg)
+document.getElementById("sendChatMsg").addEventListener("click", function () {
+  sendmsg();
+});
+document.getElementById("playername", function () {
+  join(document.getElementById("playername").value);
+});
+document.getElementById("chatmessage").addEventListener("click", function () {
+  sendmsg();
+});
 function diceEventListeners() {
-    for (let i = 0; i < document.getElementsByClassName('dice').length; i++) {
-        document.getElementsByClassName('dice')[i].addEventListener('click', function () {
-            console.log(ismyturn,diceLocal[i].avalible)
-            if(ismyturn && diceLocal[i].avalible){
-            if (diceindex.includes(i)) {
-                diceindex = diceindex.filter(j => j != i)
-                document.getElementById('d' + i).style.backgroundColor = 'grey'
-
-            } else {
-                diceindex.push(i)
-                document.getElementById('d' + i).style.backgroundColor = 'white'
-            }}
-        })
-    }
+  for (let i = 0; i < document.getElementsByClassName("dice").length; i++) {
+    document
+      .getElementsByClassName("dice")
+      [i].addEventListener("click", function () {
+        console.log(ismyturn, diceLocal[i].avalible);
+        if (ismyturn && diceLocal[i].avalible) {
+          if (diceindex.includes(i)) {
+            diceindex = diceindex.filter((j) => j != i);
+            document.getElementById("d" + i).style.backgroundColor = "grey";
+          } else {
+            diceindex.push(i);
+            document.getElementById("d" + i).style.backgroundColor = "white";
+          }
+        }
+      });
+  }
 }
-
-
